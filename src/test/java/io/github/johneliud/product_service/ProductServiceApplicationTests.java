1d8d49a42aa8941339f78e3a946cb982ca5df231
John <johneliud4@gmail.com>
2026-02-07 10:34:09 +0300

    initial commit

---
+++ src/test/java/io/github/johneliud/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.johneliud.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
