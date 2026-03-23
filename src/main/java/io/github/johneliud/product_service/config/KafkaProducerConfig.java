72c665ad09d7fd06ae064685bbe6cf8d2a03b878
John <johneliud4@gmail.com>
2026-03-23 17:18:12 +0300

    chore(product-service): add default values to Kafka config properties for local development fallback.

--- src/main/java/io/github/johneliud/product_service/config/KafkaProducerConfig.java
+++ src/main/java/io/github/johneliud/product_service/config/KafkaProducerConfig.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
     private String bootstrapServers;
 
     @Bean
